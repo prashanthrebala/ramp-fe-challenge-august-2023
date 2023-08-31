25f24ff3184f96c33ca3a0f7fcd8f8a91d72f44c
Prashanth Rebala <prashanth.rebala@gmail.com>
2023-08-31 15:56:57 -0400

    Bug7

--- src/components/Transactions/TransactionPane.tsx
+++ src/components/Transactions/TransactionPane.tsx
@@ -1,41 +1,56 @@
-import { useState } from "react"
-import { InputCheckbox } from "../InputCheckbox"
-import { TransactionPaneComponent } from "./types"
+import { useEffect, useState } from "react";
+import { InputCheckbox } from "../InputCheckbox";
+import { TransactionPaneComponent } from "./types";
+import { useCustomFetch } from "src/hooks/useCustomFetch";
 
 export const TransactionPane: TransactionPaneComponent = ({
-  transaction,
-  loading,
-  setTransactionApproval: consumerSetTransactionApproval,
+	transaction,
+	loading,
+	setTransactionApproval: consumerSetTransactionApproval,
 }) => {
-  const [approved, setApproved] = useState(transaction.approved)
+	const [approved, setApproved] = useState(transaction.approved);
+	const [updateApproval, setUpdateApproval] = useState(false);
+	const { clearCacheByEndpoint } = useCustomFetch();
+	useEffect(() => {
+		const updateTransactionApproval = async () => {
+			await consumerSetTransactionApproval({
+				transactionId: transaction.id,
+				newValue: approved,
+			});
 
-  return (
-    <div className="RampPane">
-      <div className="RampPane--content">
-        <p className="RampText">{transaction.merchant} </p>
-        <b>{moneyFormatter.format(transaction.amount)}</b>
-        <p className="RampText--hushed RampText--s">
-          {transaction.employee.firstName} {transaction.employee.lastName} - {transaction.date}
-        </p>
-      </div>
-      <InputCheckbox
-        id={transaction.id}
-        checked={approved}
-        disabled={loading}
-        onChange={async (newValue) => {
-          await consumerSetTransactionApproval({
-            transactionId: transaction.id,
-            newValue,
-          })
+			clearCacheByEndpoint(["paginatedTransactions", "transactionsByEmployee"]);
+		};
 
-          setApproved(newValue)
-        }}
-      />
-    </div>
-  )
-}
+		if (updateApproval) {
+			updateTransactionApproval();
+			setUpdateApproval(false);
+		}
+	}, [approved]);
+
+	return (
+		<div className="RampPane">
+			<div className="RampPane--content">
+				<p className="RampText">{transaction.merchant} </p>
+				<b>{moneyFormatter.format(transaction.amount)}</b>
+				<p className="RampText--hushed RampText--s">
+					{transaction.employee.firstName} {transaction.employee.lastName} -{" "}
+					{transaction.date}
+				</p>
+			</div>
+			<InputCheckbox
+				id={transaction.id}
+				checked={approved}
+				disabled={loading}
+				onChange={async (newValue) => {
+					setApproved(newValue);
+					setUpdateApproval(true);
+				}}
+			/>
+		</div>
+	);
+};
 
 const moneyFormatter = new Intl.NumberFormat("en-US", {
-  style: "currency",
-  currency: "USD",
-})
+	style: "currency",
+	currency: "USD",
+});
